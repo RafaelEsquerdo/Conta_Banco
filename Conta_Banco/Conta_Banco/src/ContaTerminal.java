7d1f4ae6a1b6871e18ae88cd574132437b54fa7d
Rafael Esquerdo <rafasesquerdo@gmail.com>
2024-08-28 15:45:44 -0300

    BancoTerminal

--- Conta_Banco/Conta_Banco/src/ContaTerminal.java
+++ Conta_Banco/Conta_Banco/src/ContaTerminal.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
 public class ContaTerminal
  {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      int numeroConta;
+      String agenciaBanco;
+      String nomeCliente;
+      double saldo=237.48;
+                    
+        Scanner conta = new Scanner(System.in);
+        System.out.println("======================================");
+        System.out.println("Registro Bancário");
+        System.out.println("======================================");
+
+         System.out.println("Por Favor, digite p número da Agencia");
+         numeroConta = conta.nextInt();
+
+         System.out.println("Qual a sua agencia?");
+         agenciaBanco=conta.next();
+
+         System.out.println("Qual o seu nome?");
+         nomeCliente=conta.next();
+        System.out.println("=====================================");
+         System.out.println("Olá, " + nomeCliente + " Seja bem-vindo e obrigado por escolher noss banco, sua agencia é: " + agenciaBanco+" conta"+numeroConta+" e o seu saldo atual é de: " + saldo+" ele já está disponível para saque.");
+         System.out.println("=======================================");
+
+
     }
 }
